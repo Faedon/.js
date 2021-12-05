7140ad9571f4f950f8b07d2f3af155738bf0da3a
Emre Kaan Satış <redbirdstwo@hotmail.com>
2021-12-06 00:29:16 +0300

    2.0

--- Learning Js/Blackjack/game.js
+++ Learning Js/Blackjack/game.js
@@ -1,16 +1,32 @@
-let num1 = Math.floor(Math.random() * 10) + 2
-let num2 = Math.floor(Math.random() * 10) + 2
+let firstCard = Math.floor(Math.random() * 20) + 2
+let secondCard = Math.floor(Math.random() * 10) + 2
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let gameStatus = true
-let sum = num1 + num2
+let message = ""
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
 
-if (sum<21) {
-    console.log("Do you want to draw?")
-} else if (sum === 21) {
-    console.log("Blackjack!")
-    hasBlackJack = true
-} else {
-    console.log("You lost!")
-    gameStatus = false
+function start() {
+    if (sum<21) {
+        message ="Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You lost!"
+        gameStatus = false
+    }
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " - " + secondCard
+}
+
+function newCard() {
+    messageEl.textContent = "Drawing a new card from the deck!"
+    let card = 5
+    sum += card
+    start()
 }
